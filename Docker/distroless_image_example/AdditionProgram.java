1bb436279a0c4240cba2078f39415325b7436959
Ubuntu <ubuntu@ip-172-31-12-78.eu-north-1.compute.internal>
2025-03-26 05:44:09 +0000

    Basic Java addition program using Distroless image

---
+++ Docker/distroless_image_example/AdditionProgram.java
@@ -0,0 +1,12 @@
+public class AdditionProgram {
+    public static void main(String[] args) {
+	
+        double num1 = 5.4;
+        double num2 = 3.6;
+
+        double sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
